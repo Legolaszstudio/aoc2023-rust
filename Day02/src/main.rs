84399b67c58b2a6694def95ce6b0336fafac4706
Legolaszstudio <novotny.levente@gmail.com>
2023-12-03 14:26:15 +0100

    Day02 Part2

--- Day02/src/main.rs
+++ Day02/src/main.rs
@@ -7,9 +7,9 @@ fn main() {
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let parts = contents.split("\r\n");
+
     let balls_available = vec![12, 13, 14];
     let mut sum = 0;
-
 
     for line in parts {
         let games: Vec<_> = line.split("Game ").collect();
@@ -34,16 +34,56 @@ fn main() {
                     ball_counts[2] += ball_count;
                 }
             }
-            if (ball_counts[0] > balls_available[0]) || (ball_counts[1] > balls_available[1]) || (ball_counts[2] > balls_available[2]) {
-                println!("Game {} is not possible", game_id_int);
+            if (ball_counts[0] > balls_available[0])
+                || (ball_counts[1] > balls_available[1])
+                || (ball_counts[2] > balls_available[2])
+            {
+                //println!("Game {} is not possible", game_id_int);
                 possible = false;
                 break;
             }
         }
         if possible {
-            println!("Game {} is possible", game_id_int);
+            //println!("Game {} is possible", game_id_int);
             sum += game_id_int;
         }
     }
     println!("Sum: {}", sum);
+
+    partTwo(contents.split("\r\n"));
+}
+
+fn partTwo(lines: std::str::Split<'_, &str>) {
+    let mut sum = 0;
+    for line in lines {
+        let games: Vec<_> = line.split("Game ").collect();
+        let games: Vec<_> = games[1].split(": ").collect();
+
+        let games: Vec<_> = games[1].split("; ").collect();
+        let mut ball_counts = vec![0, 0, 0];
+        for game in games {
+            let balls: Vec<_> = game.split(", ").collect();
+
+            for ball in balls {
+                let ball: Vec<_> = ball.split(" ").collect();
+                let ball_count = ball[0].parse::<i32>().unwrap();
+                let ball_color = ball[1];
+                if ball_color == "red" {
+                    if ball_counts[0] < ball_count {
+                        ball_counts[0] = ball_count;
+                    }
+                } else if ball_color == "green" {
+                    if ball_counts[1] < ball_count {
+                        ball_counts[1] = ball_count;
+                    }
+                } else if ball_color == "blue" {
+                    if ball_counts[2] < ball_count {
+                        ball_counts[2] = ball_count;
+                    }
+                }
+            }
+        }
+        sum += ball_counts[0] * ball_counts[1] * ball_counts[2];
+    }
+    println!("Sum2: {}", sum);
 }
